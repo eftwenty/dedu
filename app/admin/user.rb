e714159ea92636f963a475bc5e0575c3589eb9a7
eftwenty <ftwnt20@gmail.com>
2017-06-04 23:28:21 +0300

    ActiveAdmin, theme, basic users

---
+++ app/admin/user.rb
@@ -0,0 +1,33 @@
+ActiveAdmin.register User do
+  permit_params :email, :role, :password, :password_confirmation
+
+  index do
+    selectable_column
+    id_column
+    column :email
+    column :role
+    column :current_sign_in_at
+    column :sign_in_count
+    column :created_at
+    actions
+  end
+
+  filter :email
+  filter :role
+  filter :current_sign_in_at
+  filter :sign_in_count
+  filter :created_at
+
+  form do |f|
+    f.inputs "Admin Details" do
+      f.input :email
+      f.input :role
+      if f.object.new_record?
+        f.input :password
+        f.input :password_confirmation
+      end
+    end
+    f.actions
+  end
+
+end
---
+++ app/models/user.rb
@@ -0,0 +1,10 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, 
+         :recoverable, :rememberable, :trackable, :validatable
+
+  ROLES = { super: 0, teacher: 1, student: 2 }
+
+  enum role: ROLES
+end
