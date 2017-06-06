69fec9ee0f92363efdea9a43d40c571c14b3ca31
eftwenty <ftwnt20@gmail.com>
2017-06-07 02:45:25 +0300

    updated ssed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-User.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password')
+User.super.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password')
 
 30.times do Course.create(title: Faker::Job.field, description: Faker::Lorem.paragraph)  end
 
